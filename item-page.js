f6d9010bc450f02ba14f8afb48da3c77b4d6c7ed
Tiago Rafael <161669844+tiagorafael404@users.noreply.github.com>
2026-07-15 21:30:53 +0100

    a

--- item-page.js
+++ item-page.js
@@ -53,6 +53,8 @@
 
   function loadProductItemsFromJson() {
     var itemsUrl = getItemsJsonUrl();
+    var pathName = window.location.pathname.toLowerCase();
+    var isIndexPage = pathName === "/" || pathName.endsWith("/index.html") || pathName.endsWith("index.html");
 
     fetch(itemsUrl)
       .then(function (response) {
@@ -90,61 +92,140 @@
         var productsContainer = document.querySelector(".products");
         var productsList = productsContainer ? productsContainer.querySelector("ul") : null;
 
-        if (productsContainer && !productsList) {
-          productsList = document.createElement("ul");
-          productsContainer.appendChild(productsList);
+        function compareByNameThenId(a, b) {
+          var nameA = (a && a.name ? a.name : "").trim();
+          var nameB = (b && b.name ? b.name : "").trim();
+          var byName = nameA.localeCompare(nameB, "pt", { sensitivity: "base" });
+
+          if (byName !== 0) {
+            return byName;
+          }
+
+          return Number(a.id || 0) - Number(b.id || 0);
         }
 
-        if (productsList) {
-          while (productsList.children.length < filteredItems.length) {
-            var li = document.createElement("li");
-            li.innerHTML =
-              '<a class="product-link" href="#">'
-              + '<div class="image"></div>'
-              + "</a>"
-              + '<div class="info">'
-              + '<div class="name"><a class="name-link" href="#"></a></div>'
-              + '<div class="price"><a class="price-text"></a></div>'
-              + "</div>";
-            productsList.appendChild(li);
+        function fillProductLi(li, item) {
+          var productLink = li.querySelector(".product-link");
+          var nameLink = li.querySelector(".name-link");
+          var priceText = li.querySelector(".price-text");
+          var imageDiv = li.querySelector(".image");
+
+          if (productLink) {
+            productLink.href = item.url || "#";
           }
 
-          while (productsList.children.length > filteredItems.length) {
-            productsList.removeChild(productsList.lastElementChild);
+          if (nameLink) {
+            nameLink.href = item.url || "#";
+            nameLink.textContent = item.name || "";
           }
 
-          Array.from(productsList.children).forEach(function (li, index) {
-            var item = filteredItems[index] || null;
+          if (priceText) {
+            priceText.textContent = item.price || "";
+          }
 
-            if (!item) {
-              li.style.display = "none";
-              return;
+          if (imageDiv) {
+            imageDiv.style.backgroundImage = item.image ? "url('" + item.image + "')" : "none";
+          }
+        }
+
+        function createProductLi(item) {
+          var li = document.createElement("li");
+          li.innerHTML =
+            '<a class="product-link" href="#">'
+            + '<div class="image"></div>'
+            + "</a>"
+            + '<div class="info">'
+            + '<div class="name"><a class="name-link" href="#"></a></div>'
+            + '<div class="price"><a class="price-text"></a></div>'
+            + "</div>";
+
+          fillProductLi(li, item);
+          return li;
+        }
+
+        if (isIndexPage && productsContainer) {
+          var groupedByGolf = filteredItems.reduce(function (acc, item) {
+            var golfKey = item.golf !== undefined && item.golf !== null ? String(item.golf) : "Outros";
+
+            if (!acc[golfKey]) {
+              acc[golfKey] = [];
             }
 
-            li.style.display = "";
+            acc[golfKey].push(item);
+            return acc;
+          }, {});
 
-            var productLink = li.querySelector(".product-link");
-            var nameLink = li.querySelector(".name-link");
-            var priceText = li.querySelector(".price-text");
-            var imageDiv = li.querySelector(".image");
+          var orderedGolfKeys = Object.keys(groupedByGolf).sort(function (a, b) {
+            var numberA = Number(a);
+            var numberB = Number(b);
+            var isNumberA = !Number.isNaN(numberA);
+            var isNumberB = !Number.isNaN(numberB);
 
-            if (productLink) {
-              productLink.href = item.url || "#";
+            if (isNumberA && isNumberB) {
+              return numberA - numberB;
             }
 
-            if (nameLink) {
-              nameLink.href = item.url || "#";
-              nameLink.textContent = item.name || "";
+            if (isNumberA) {
+              return -1;
             }
 
-            if (priceText) {
-              priceText.textContent = item.price || "";
+            if (isNumberB) {
+              return 1;
             }
 
-            if (imageDiv && item.image) {
-              imageDiv.style.backgroundImage = "url('" + item.image + "')";
-            }
+            return a.localeCompare(b, "pt", { sensitivity: "base" });
           });
+
+          productsContainer.innerHTML = "";
+
+          orderedGolfKeys.forEach(function (golfKey) {
+            var groupSection = document.createElement("section");
+            groupSection.className = "products-golf-group";
+
+            var groupTitle = document.createElement("h2");
+            groupTitle.className = "products-golf-title";
+            groupTitle.textContent = golfKey === "Outros" ? "Outros" : "Golf " + golfKey;
+
+            var groupList = document.createElement("ul");
+
+            groupedByGolf[golfKey]
+              .slice()
+              .sort(compareByNameThenId)
+              .forEach(function (item) {
+                groupList.appendChild(createProductLi(item));
+              });
+
+            groupSection.appendChild(groupTitle);
+            groupSection.appendChild(groupList);
+            productsContainer.appendChild(groupSection);
+          });
+        } else {
+          if (productsContainer && !productsList) {
+            productsList = document.createElement("ul");
+            productsContainer.appendChild(productsList);
+          }
+
+          if (productsList) {
+            while (productsList.children.length < filteredItems.length) {
+              productsList.appendChild(createProductLi({}));
+            }
+
+            while (productsList.children.length > filteredItems.length) {
+              productsList.removeChild(productsList.lastElementChild);
+            }
+
+            Array.from(productsList.children).forEach(function (li, index) {
+              var item = filteredItems[index] || null;
+
+              if (!item) {
+                li.style.display = "none";
+                return;
+              }
+
+              li.style.display = "";
+              fillProductLi(li, item);
+            });
+          }
         }
 
         function normalizeItemReference(reference) {
