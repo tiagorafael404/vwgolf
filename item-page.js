2623222f2988b0507ff5e1177d9ad0229c759e11
Tiago Rafael <161669844+tiagorafael404@users.noreply.github.com>
2026-07-22 20:09:38 +0100

    Update item-page.js

--- item-page.js
+++ item-page.js
@@ -24,6 +24,38 @@
       return;
     }
 
+    var unitedStatesTimeZones = [
+      "America/New_York",
+      "America/Detroit",
+      "America/Kentucky/Louisville",
+      "America/Kentucky/Monticello",
+      "America/Indiana/Indianapolis",
+      "America/Indiana/Vincennes",
+      "America/Indiana/Winamac",
+      "America/Indiana/Marengo",
+      "America/Indiana/Petersburg",
+      "America/Indiana/Vevay",
+      "America/Chicago",
+      "America/Indiana/Tell_City",
+      "America/Indiana/Knox",
+      "America/Menominee",
+      "America/North_Dakota/Center",
+      "America/North_Dakota/New_Salem",
+      "America/North_Dakota/Beulah",
+      "America/Denver",
+      "America/Boise",
+      "America/Phoenix",
+      "America/Los_Angeles",
+      "America/Anchorage",
+      "America/Juneau",
+      "America/Sitka",
+      "America/Metlakatla",
+      "America/Yakutat",
+      "America/Nome",
+      "America/Adak",
+      "Pacific/Honolulu"
+    ];
+
     var localeCandidates = [];
     if (Array.isArray(navigator.languages)) {
       localeCandidates = localeCandidates.concat(navigator.languages);
@@ -32,10 +64,21 @@
       localeCandidates.push(navigator.language);
     }
 
-    var isUnitedStates = localeCandidates.some(function (locale) {
+    var hasUsLocale = localeCandidates.some(function (locale) {
       var normalizedLocale = String(locale || "").toLowerCase();
       return normalizedLocale === "en-us" || normalizedLocale.endsWith("-us");
     });
+
+    var resolvedTimeZone = "";
+    try {
+      resolvedTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone || "";
+    } catch (error) {
+      resolvedTimeZone = "";
+    }
+
+    var isUnitedStates = resolvedTimeZone
+      ? unitedStatesTimeZones.indexOf(resolvedTimeZone) !== -1
+      : hasUsLocale;
 
     var now = new Date();
     var inTwoWeeks = new Date(now);
